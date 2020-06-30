00a36f5cac977663f8b70ce76f16c8c6da376e04
Ahmed Gamal <agmghazi@hotmail.com>
2020-07-01 02:00:49 +0300

    add db context in Models and create dao for Users

---
+++ CashOnline.Models/Dao/UserDao.cs
@@ -0,0 +1,39 @@
+﻿using CashOnline.Models.EF;
+using System.Linq;
+
+namespace CashOnline.Models.Dao
+{
+
+    public class UserDao
+    {
+        private readonly CashOnlineDbContext _dbContext = null;
+
+        public UserDao()
+        {
+            _dbContext = new CashOnlineDbContext();
+        }
+
+
+        public long Insert(User entity)
+        {
+            _dbContext.Users.Add(entity);
+            _dbContext.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Login(string userName, string passWord)
+        {
+            var result = _dbContext.Users
+                .Count(x => x.UserName == userName && x.Password == passWord);
+            if (result > 0)
+            {
+                return true; ;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
